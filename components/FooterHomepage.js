b5368740ae55d2d7af7180fb4e152a98d0433bf9
Ayush Saini <ayushsainiiitb00@gmail.com>
2022-01-17 16:10:06 +0530

    link to github readme to guide on how to add a new page

--- components/FooterHomepage.js
+++ components/FooterHomepage.js
@@ -4,7 +4,14 @@ const FooterHomepage = () => {
   return (
     <footer>
       <p>
-        Built by <a href="https://twitter.com/AyushCodes">Ayush</a>
+        Built by <a href="https://twitter.com/AyushCodes">Ayush</a> | Add your{' '}
+        <a
+          href="https://github.com/AyushSaini00/book-in-3#how-can-i-add-my-page-"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          page
+        </a>
         {/* <Link href="/why">
           <a>Why?</a>
         </Link> */}
--- components/FooterUserPage.js
+++ components/FooterUserPage.js
@@ -3,6 +3,16 @@ import Link from 'next/link';
 const FooterUserPage = () => {
   return (
     <footer>
+      <p className="footer-user-page-p-1">
+        Add your{' '}
+        <a
+          href="https://github.com/AyushSaini00/book-in-3#how-can-i-add-my-page-"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          page
+        </a>
+      </p>
       <p>
         Powered by{' '}
         <Link href="/">
