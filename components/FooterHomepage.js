0a0956ab1fdde0cd3ef053631547a98d850be642
Ayush Saini <ayushsainiiitb00@gmail.com>
2023-01-26 16:15:24 +0530

    common footer change

--- components/FooterHomepage.js
+++ components/FooterHomepage.js
@@ -4,14 +4,15 @@ const FooterHomepage = () => {
   return (
     <footer>
       <p>
-        Built by <a href="https://twitter.com/AyushCodes">Ayush</a> | Add your{' '}
+        Add your{' '}
         <a
           href="https://github.com/AyushSaini00/book-in-3#how-can-i-add-my-page-"
           target="_blank"
           rel="noopener noreferrer"
         >
           page
-        </a>
+        </a>{' '}
+        | Built by <a href="https://twitter.com/AyushCodes">Ayush</a>
         {/* <Link href="/why">
           <a>Why?</a>
         </Link> */}
